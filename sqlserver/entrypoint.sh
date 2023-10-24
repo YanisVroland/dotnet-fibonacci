ddb4dc1d70cd9fa6ad6ed371f9ea03b94751de61
Yanis <yanis.vroland@gmail.com>
2023-10-24 15:26:34 +0200

    add sqlbd

---
+++ sqlserver/entrypoint.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+set -e
+if [ "$1" = '/opt/mssql/bin/sqlservr' ]; then
+  if [ ! -f /tmp/app-initialized ]; then
+    function initialize_app_database() {
+      sleep 40s
+      /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Your_password123 -d master -i ./initBase.sql
+      touch /tmp/app-initialized
+    }
+    initialize_app_database & 
+  fi
+fi
+
+exec "$@"
---
+++ sqlserver/initBase.sql
@@ -0,0 +1,12 @@
+﻿USE [master]
+CREATE DATABASE FibonacciData
+GO 
+USE FibonacciData
+GO
+CREATE SCHEMA sch_fib AUTHORIZATION [dbo];
+GO
+CREATE TABLE [sch_fib].[T_Fibonacci](
+    [FIB_Id] uniqueidentifier NOT NULL DEFAULT newid(),
+    [FIB_ Input] [int] NOT NULL,
+    [FIB_ Output] [bigint] NOT NULL,
+    CONSTRAINT [PK_Fibonacci] PRIMARY KEY CLUSTERED ([FIB_Id]))
